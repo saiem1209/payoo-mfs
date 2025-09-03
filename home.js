fa437c0f05b885cb1a3bef94d885f2f115106a94
saiem1209 <saiemhossain2001@gmail.com>
2025-09-03 19:50:27 +0600

    done

--- home.js
+++ home.js
@@ -1,11 +1,22 @@
 
+const pin = 1234;
 
 document.getElementById('admoney-btn').addEventListener('click', function(e){
     e.preventDefault();
     const bank = document.getElementById('bank').value;
     const account = document.getElementById('bank-number').value;
+    
     const addAmount = parseInt(document.getElementById('add-amount').value);
+    if(addAmount <= 0){
+        alert('enter valid amount');
+        return;
+    }
     const pinNumber = document.getElementById('pin-number').value;
+    if( pin != pinNumber ){
+        alert('Please Enter Valid Pin!');
+        return;
+
+    }
 
     
 
